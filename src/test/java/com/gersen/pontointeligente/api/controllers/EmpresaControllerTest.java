a55073c7462a0db3e23110f3ed53ae76e166a464
Gersen James <gersen.jorge@gmail.com>
2019-02-15 09:48:17 -0200

    Controller de empresas

---
+++ src/test/java/com/gersen/pontointeligente/api/controllers/EmpresaControllerTest.java
@@ -0,0 +1,84 @@
+package com.gersen.pontointeligente.api.controllers;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.gersen.pontointeligente.api.entities.Empresa;
+import com.gersen.pontointeligente.api.services.EmpresaService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc // anotação para contexto web
+@ActiveProfiles("test")
+public class EmpresaControllerTest {
+
+	@Autowired
+	private MockMvc mvc; // container web, indicado acima, no contexto web
+	                     // permite fazer requisições http no modo teste
+
+	@MockBean
+	private EmpresaService empresaService; // criado mock da empresa porque ela já foi testada,
+	// aqui queremos testar o controller em si
+
+	private static final String BUSCAR_EMPRESA_CNPJ_URL = "/api/empresas/cnpj/";
+	private static final Long ID = Long.valueOf(1);
+	private static final String CNPJ = "51463645000100"; // qualquer cnpj valido (qqcoisa, busque no google
+	private static final String RAZAO_SOCIAL = "Empresa XYZ";
+
+	@Test
+	@WithMockUser
+	public void testBuscarEmpresaCnpjInvalido() throws Exception {
+		BDDMockito.given(this.empresaService.buscarPorCnpj(Mockito.anyString())).willReturn(Optional.empty());
+        // simulando que ele não vai encontrar
+		
+		mvc.perform(MockMvcRequestBuilders.get(BUSCAR_EMPRESA_CNPJ_URL + CNPJ).accept(MediaType.APPLICATION_JSON))
+		// mvc.perform quer dizer execute, e esse mvc veio do MockMvc acima
+				.andExpect(status().isBadRequest()) // garantir que seja 400 - bad request
+				.andExpect(jsonPath("$.errors").value("Empresa não encontrada para o CNPJ " + CNPJ));
+		                 // jsonPath permite fazer query em json
+						 // errors é o array de erros do postamn
+	}
+
+	@Test
+	@WithMockUser
+	public void testBuscarEmpresaCnpjValido() throws Exception {
+		BDDMockito.given(this.empresaService.buscarPorCnpj(Mockito.anyString()))
+				.willReturn(Optional.of(this.obterDadosEmpresa()));
+				// retorna uma empresa valida
+
+		mvc.perform(MockMvcRequestBuilders.get(BUSCAR_EMPRESA_CNPJ_URL + CNPJ)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.data.id").value(ID))
+				.andExpect(jsonPath("$.data.razaoSocial", equalTo(RAZAO_SOCIAL)))
+				.andExpect(jsonPath("$.data.cnpj", equalTo(CNPJ)))
+				.andExpect(jsonPath("$.errors").isEmpty());
+	}
+
+	private Empresa obterDadosEmpresa() {
+		Empresa empresa = new Empresa();
+		empresa.setId(ID);
+		empresa.setRazaoSocial(RAZAO_SOCIAL);
+		empresa.setCnpj(CNPJ);
+		return empresa;
+	}
+
+}
